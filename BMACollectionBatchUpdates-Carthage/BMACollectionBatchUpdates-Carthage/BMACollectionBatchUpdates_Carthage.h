e0747af7557eef80e113c55d7f071425006de682
alexander.balaban <alexander.balaban@corp.badoo.com>
2018-02-01 15:22:27 +0000

    carthage support

---
+++ BMACollectionBatchUpdates-Carthage/BMACollectionBatchUpdates-Carthage/BMACollectionBatchUpdates_Carthage.h
@@ -0,0 +1,20 @@
+//
+//  BMACollectionBatchUpdates_Carthage.h
+//  BMACollectionBatchUpdates-Carthage
+//
+//  Created by alexander.balaban on 01/02/2018.
+//  Copyright © 2018 badoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMACollectionBatchUpdates_Carthage.
+FOUNDATION_EXPORT double BMACollectionBatchUpdates_CarthageVersionNumber;
+
+//! Project version string for BMACollectionBatchUpdates_Carthage.
+FOUNDATION_EXPORT const unsigned char BMACollectionBatchUpdates_CarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMACollectionBatchUpdates_Carthage/PublicHeader.h>
+#import <BMACollectionBatchUpdates_Carthage/BMACollectionUpdates.h>
+#import <BMACollectionBatchUpdates_Carthage/UICollectionView+BMABatchUpdates.h>
+#import <BMACollectionBatchUpdates_Carthage/UITableView+BMABatchUpdates.h>
