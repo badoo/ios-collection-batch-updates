4a485e5a7604d9cd84289b161d383aa75c4e46f8
alexander.balaban <alexander.balaban@corp.badoo.com>
2018-02-01 15:41:12 +0000

    rename target

---
+++ BMACollectionBatchUpdates-Carthage/BMACollectionBatchUpdates-Carthage/BMACollectionBatchUpdates.h
@@ -0,0 +1,20 @@
+//
+//  BMACollectionBatchUpdates.h
+//  BMACollectionBatchUpdates
+//
+//  Created by alexander.balaban on 01/02/2018.
+//  Copyright © 2018 badoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BMACollectionBatchUpdates.
+FOUNDATION_EXPORT double BMACollectionBatchUpdatesVersionNumber;
+
+//! Project version string for BMACollectionBatchUpdates.
+FOUNDATION_EXPORT const unsigned char BMACollectionBatchUpdatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BMACollectionBatchUpdates/PublicHeader.h>
+#import <BMACollectionBatchUpdates/BMACollectionUpdates.h>
+#import <BMACollectionBatchUpdates/UICollectionView+BMABatchUpdates.h>
+#import <BMACollectionBatchUpdates/UITableView+BMABatchUpdates.h>
